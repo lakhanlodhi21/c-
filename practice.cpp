0de4be737ec9a9f45da07673bc2393c2604f2130
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-20 12:39:14 +0530

    updated

--- practice.cpp
+++ practice.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
-using namespace std ;
-int main(){
+#include <iostream>
+using namespace std;
+int main()
+{
 
     // int a,  f = 1;
     // cout << "enter a number";
     // cin >> a;
-    
+
     // while (b>1)
     // {
     //     if (a%b==0)
@@ -23,22 +24,22 @@ int main(){
     // {
     //     cout << "prime number";
     // }
-// for (int i = 2; i < a; i++)
-// {
-//    if (a%i==0)
-//    {
-//        f = 100;
-//        break;
-//    }
-   
-// }
-// if (f==100)
-// {
-//     cout << "its not a prime";
-// }
-// else{
-//     cout << "prime number";
-// }
+    // for (int i = 2; i < a; i++)
+    // {
+    //    if (a%i==0)
+    //    {
+    //        f = 100;
+    //        break;
+    //    }
+
+    // }
+    // if (f==100)
+    // {
+    //     cout << "its not a prime";
+    // }
+    // else{
+    //     cout << "prime number";
+    // }
 
     // int a, b = 1;
     // cout << "enter a number";
@@ -50,31 +51,33 @@ int main(){
     // } while (a>=1);
     // cout << b;
 
-//     int a = 10;
-//     int b = a;
-    
-//     while (a>=1)
-//     {
-//        if (b%a == 0)
-//        {
-//            cout<< a<<"\n";
-//        }
-//        a--;
-//     }
-    
-// }
+    //     int a = 10;
+    //     int b = a;
 
-// int num;
-// cout << "enter a number";
-// int reverse = 0;
-// cin >> num;
-// while (num!=0)
-// {
-//     int digit = num % 10;
-//     reverse = reverse * 10 + digit;
-//     num = num / 10;
-// }
-// cout << reverse << "\n";
+    //     while (a>=1)
+    //     {
+    //        if (b%a == 0)
+    //        {
+    //            cout<< a<<"\n";
+    //        }
+    //        a--;
+    //     }
+
+    // }
+
+    // int num;
+    // cout << "enter a number";
+    // int reverse = 0;
+    // cin >> num;
+    // while (num!=0)
+    // {
+    //     int digit = num % 10;
+    //     reverse = reverse * 10 + digit;
+    //     num = num / 10;
+    // }
+    // cout << reverse << "\n";
+
+    //-------------->>>>>>>>> factorial program <<<<<<<<<<<<------------------\\
 
     // int a;
     // cout << "enter a number";
@@ -83,9 +86,126 @@ int main(){
     // for (int i = 1; i <= a; i++)
     // {
     //     sum = sum * i;
-    //     cout << sum;
+    //     cout << sum <<"\n";
     // }
 
-    
+    //-------------->>>>>>>>> factors program <<<<<<<<<<<<------------------\\
 
+    // int a = 15;
+    // int b = a;
+    // int count = 0;
+    // while (a>=1)
+    // {
+    //    if (b%a== 0)
+    //    {
+    //        cout << a <<"\n";
+    //        if (b % a == 0)
+    //        {
+    //            count = count + 1;
+    //        }
+
+    //    }
+    //    a--;
+    // }
+
+    // cout << count;
+
+    //-------------->>>>>>>>> print tabel program <<<<<<<<<<<<------------------\\
+
+    // int a, b = 1;
+    // cout << "enter a number";
+    // cin >> a;
+    // while (b<=10)
+    // {
+    //     cout << a * b<<"\n";
+    //     b++;
+
+    // }
+
+    //-------------->>>>>>>>> reverse number program <<<<<<<<<<<<------------------\\
+
+    // int num;
+    // int re = 0;
+    // cout << "enter a number";
+    // cin >> num;
+    // while (num != 0)
+    // {
+    //     int digit = num % 10;
+    //     re = re * 10 + digit;
+    //     num = num / 10;
+    // }
+    // cout << re;
+
+    //-------------->>>>>>>>> count digits program <<<<<<<<<<<<------------------\\
+
+    // int num;
+    // int count = 0;
+    // cout << "enter number";
+    // cin >> num;
+    // while (num != 0)
+    // {
+    //     num = num / 10;
+    //     count++;
+
+    // }
+    // cout << count;
+
+    // int num, orignal, digit, re = 0;
+    // cout << "enter a number";
+    // cin >> num;
+    // orignal = num;
+    // while (num != 0)
+    // {
+    //     digit = num % 10;
+    //     re = re * 10 + digit;
+    //     num = num / 10;
+    // }
+
+    //     if (orignal==re)
+    //     {
+    //         cout << "its palindrome";
+    //     }
+    //     else
+    //     {
+    //         cout<<"not palindrome";
+    //     }
+
+    // int num;
+    // int sum = 0;
+    // cout << "enter a number";
+    // cin >> num;
+    // while (num != 0)
+    // {
+    //     int digit = num % 10;
+    //     sum = sum + digit;
+    //     num = num / 10;
+    // }
+    // cout << sum;
+
+    // int a;
+    // cout << "enter a number";
+    // cin >> a;
+    // int b = a - 1;
+    // int f = 1;
+
+    // while (b>1)
+    // {
+    //    if (a%b==0)
+    //    {
+    //        f = 2;
+    //        break;
+    //    }
+    //    b--;
+    //     }
+    //     if (f == 2)
+    //     {
+    //         cout << "not prime";
+    //     }
+    //     else{
+    //         cout << "prime";
+    //     }
+
+
+
+    
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+  int a;
+    cout << "enter a number";
+    cin >> a;
+    int sum = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        sum = sum * i;
+        cout << sum;
+    }
